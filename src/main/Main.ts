cfae504567bb85fd39dafcba45666da7a6802651
Max Zinovik <zinovik@gmail.com>
2024-01-01 18:34:28 +0100

    remove console.logs

--- src/main/Main.ts
+++ src/main/Main.ts
@@ -15,8 +15,6 @@ export class Main {
         const sourcesConfig = await this.dataService.getSourcesConfig();
         console.timeLog('Getting data from the data service');
 
-        console.log(sourcesConfig);
-
         console.time('Writing data to the storage service');
         await this.storageService.saveSourcesConfig(sourcesConfig);
         console.timeLog('Writing data to the storage service');
